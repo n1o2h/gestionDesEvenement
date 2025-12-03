b4a1098d7d8d2c84e0cede05a51818690c802197
n1o2h <aythmadnhylt@gmail.com>
2025-12-03 23:12:19 +0100

    insertion des 10 premiers requetes de manipulations des tableaux

--- query.sql
+++ query.sql
@@ -228,3 +228,52 @@ INSERT INTO financier_evenement (`idEvenement`, `idSponsor`) VALUES
 (19, 5),
 (19, 7);
 
+
+/* les requetes sql */
+-- Afficher tous les membres du BDE avec leur rôle et leur date d’adhésion
+SELECT * FROM membrebde;
+
+-- Lister tous les événements prévus après une date donnée
+-- SELECT * FROM evenement;
+-- SELECT * FROM evenement 
+-- WHERE dateEvenment="2025-12-29";
+
+-- Afficher les informations des événements ayant un budget supérieur à 5000 MAD
+SELECT * FROM evenement 
+WHERE budgetPrevu>= 5000;
+
+-- Lister les participants inscrits à un événement spécifique
+SELECT * from inscire_evenement;
+
+-- Afficher les sponsors qui ont contribué à des événements ayant un budget supérieur à 3500 MAD
+SELECT * FROM sponsor
+WHERE idSponsor IN (
+      SELECT idSponsor FROM financier_evenement 
+      WHERE idEvenement IN (
+            SELECT idEvenement FROM evenement
+            WHERE budgetPrevu >= 3500
+            )
+      );
+
+-- Calculer la moyenne du budget des événements organisés.
+SELECT AVG(budgetPrevu) FROM evenement WHERE budgetPrevu in (
+	SELECT budgetPrevu from evenement WHERE idEvenement in (
+		SELECT idEvenement from organiser_evenement
+		)
+      );
+
+-- Afficher le nom et la date des événements sponsorisés par un sponsor donné
+SELECT nomEvenement,dateEvenment FROM evenement WHERE idEvenement in (
+	SELECT idEvenement from financier_evenement WHERE idSponsor = 3
+);
+
+-- Lister les événements qui se déroulent dans un lieu donné.
+SELECT * from evenement WHERE lieu = "youcode";
+
+-- Afficher les 5 derniers événements organisés par le BDE.
+SELECT * FROM evenement 
+WHERE idEvenement in ( SELECT idEvenement FROM organiser_evenement)
+ORDER BY dateEvenment DESC LIMIT 5;
+
+
+
